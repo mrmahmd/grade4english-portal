ba2a3d6f2b4a5ac917e7ab8cd684fabc44a1eda1
mr.mohamedfarid <mr.mahmd@gmail.com>
2026-07-15 23:37:46 +0300

    Add files via upload

--- portal-config.js
+++ portal-config.js
@@ -1,4 +1,4 @@
 window.ENGLISH_PORTAL_CONFIG = {
   connectPlus4Url: 'https://mrmahmd.github.io/connectplus4-1term.app/',
-  english4Url: 'https://mrmahmd.github.io/english4-first-term-app/'
+  english4Url: 'https://mrmahmd.github.io/nglish4-first-term-app/'
 };
